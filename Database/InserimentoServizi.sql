efbe8fd2126928769d44174e255269e52f17e4ec
Raffaele Sais <saisraffaele08@gmail.com>
2019-06-01 23:13:57 +0200

    Add Servizi In Farmacia

--- Database/InserimentoServizi.sql
+++ Database/InserimentoServizi.sql
@@ -1,1 +1,6 @@
 -- Inserimento Servizi
+INSERT INTO Servizi(Tipo,Descrizione) VALUES("Misurazione Pressione","Misura la tua pressione gratuitamente!");
+
+INSERT INTO Servizi(Tipo,Descrizione) VALUES("Misurazione Del Peso","Misura il tuo peso e scopri il tuo peso forma gratuitamente!");
+
+INSERT INTO Servizi(Tipo,Descrizione) VALUES("Controllo Dell' Udito","Effettua il controllo dell' udito gratuitamente!");
